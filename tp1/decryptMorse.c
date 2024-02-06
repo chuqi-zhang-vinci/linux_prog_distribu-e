23868559e561a9ed67d903c3b5a2db68cbdbd7a1
张础麒 <128697483+chuqi-zhang-vinci@users.noreply.github.com>
2024-02-06 15:44:05 +0100

    decryptMorse.c à terminer

---
+++ tp1/decryptMorse.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define BUF_LEN 256
+
+static const char alpha[26][6] = {
+    ".-",   //A
+    "-...", //B
+    "-.-.", //C
+    "-..",  //D
+    ".",    //E
+    "..-.", //F
+    "--.",  //G
+    "....", //H
+    "..",   //I
+    ".---", //J
+    "-.-",  //K
+    ".-..", //L
+    "--",   //M
+    "-.",   //N
+    "---",  //O
+    ".--.", //P
+    "--.-", //Q
+    ".-.",  //R
+    "...",  //S
+    "-",    //T
+    "..-",  //U
+    "...-", //V
+    ".--",  //W
+    "-..-", //X
+    "-.--", //Y
+    "--..", //Z
+};
+
+char* decryptMorse(char* msg){
+   int size = strlen(msg);
+   char* res = malloc (size * sizeof(char));
+   int cpt = 0;
+   for (int z = 0; z < size; z++)
+   {
+      for (int i = 0; i < 26; i++)
+      {
+         for (int j = 0; j < 6; j++)
+         {
+            
+         }
+      }
+   }
+	return res;
+}
+
+char* readLine () {
+   char buf[BUF_LEN];
+   char* s = NULL;
+   bool fin = false;
+
+   while (fgets(buf, BUF_LEN, stdin)) {
+      // Traitement d'un bloc de caractères lus sur stdin
+      int taille = strlen(buf);
+      if (buf[taille-1] == '\n') {
+         taille--;
+         buf[taille] = '\0';
+         fin = true;
+      }
+
+      if (s == NULL) {
+         // Première allocation de la chaîne s
+         s = (char*) malloc((taille+1) * sizeof(char));
+         if (s == NULL) return NULL;
+         // Copie des caractères du buffer dans s
+         strcpy(s,buf);
+      } else {
+         // Réallocation de la chaîne s
+         s = (char*) realloc(s, (strlen(s)+taille+1) * sizeof(char));
+         if (s == NULL) return NULL;
+         // Concaténation des caractères du buffer à la fin de s
+         strcat(s,buf);
+      }
+
+      if (fin)
+         // Fin de la ligne lue sur stdin
+         return s;
+   }
+
+   // Cas d'erreur ou de fin de fichier
+   return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+	char* line = readLine();
+	printf("%s\n", line);
+	line = cryptMorse(line);
+	printf("%s\n", line);
+	return 0;
+}
